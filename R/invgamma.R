e0c535fe72fb84ee3c964e9e17c75bec6bdd5094
David Kahle <david.kahle@gmail.com>
2016-06-23 12:48:50 -0500

    include scale; rm hello.Rd; clean check

--- R/invgamma.R
+++ R/invgamma.R
@@ -13,9 +13,10 @@
 #' @param p vector of probabilities.
 #' @param n number of observations. If length(n) > 1, the length is
 #'   taken to be the number required.
-#' @param shape inverse gamma parameter shape
-#' @param rate inverse gamma parameter rate
-#' @param log logical; if TRUE, probabilities p are given as log(p).
+#' @param shape inverse gamma shape parameter
+#' @param rate inverse gamma rate parameter
+#' @param scale alternative to rate; scale = 1/rate
+#' @param log,log.p logical; if TRUE, probabilities p are given as log(p).
 #' @param lower.tail logical; if TRUE (default), probabilities are
 #'   P[X <= x] otherwise, P[X > x].
 #' @name invgamma
@@ -39,26 +40,33 @@ NULL
 
 #' @rdname invgamma
 #' @export
-dinvgamma <- function(x, shape, rate, log = FALSE) {
+dinvgamma <- function(x, shape, rate, scale = 1/rate, log = FALSE) {
+  if(missing(rate) && !missing(scale)) rate <- 1/scale
   log_f <- shape*log(rate) - lgamma(shape) + (-1-shape)*log(x) - rate/x
   if(log) return(log_f)
   exp(log_f)
 }
 
 
+
+
 #' @rdname invgamma
 #' @export
-pinvgamma <- function(q, shape, rate, lower.tail = TRUE) {
-  pgamma(1/q, shape, rate, lower.tail = !lower.tail)
+pinvgamma <- function(q, shape, rate, scale = 1/rate, lower.tail = TRUE, log.p = FALSE) {
+  if(missing(rate) && !missing(scale)) rate <- 1/scale
+  pgamma(1/q, shape, rate, lower.tail = !lower.tail, log.p = log.p)
 }
 # pgamma(q) = P(X <= q) = P(1/X >= 1/q) = 1 - P(1/X < 1/q)
 # P(1/X < x) = 1 - pgamma(1/x)
 
 
+
+
 #' @rdname invgamma
 #' @export
-qinvgamma <- function(p, shape, rate) {
-  qgamma(1-p, shape, rate)^(-1)
+qinvgamma <- function(p, shape, rate, scale = 1/rate, lower.tail = TRUE, log.p = FALSE) {
+  if(missing(rate) && !missing(scale)) rate <- 1/scale
+  qgamma(1-p, shape, rate, lower.tail = lower.tail, log.p = log.p)^(-1)
 }
 # P(1/X < x) = 1 - pgamma(1/x)
 # x = 1 - pgamma(1/y)
@@ -67,9 +75,12 @@ qinvgamma <- function(p, shape, rate) {
 # y = qgamma(1-x)^(-1)
 
 
+
+
 #' @rdname invgamma
 #' @export
-rinvgamma <- function(n, shape, rate) {
+rinvgamma <- function(n, shape, rate, scale = 1/rate) {
+  if(missing(rate) && !missing(scale)) rate <- 1/scale
   rgamma(n, shape, rate)^(-1)
 }
 
