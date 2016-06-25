2c8b89c2b673ccffff9c5f76a57b7ddf7d90f0c0
David Kahle <david.kahle@gmail.com>
2016-06-25 13:49:20 -0500

    add inverse chi-squared and inverse exponential distributions

---
+++ R/invchisq.R
@@ -0,0 +1,104 @@
+#' The Inverse (non-central) Chi-Squared Distribution
+#'
+#' Density, distribution function, quantile function and random
+#' generation for the inverse chi-squared distribution.
+#'
+#' The functions (d/p/q/r)invchisq simply wrap those of the standard
+#' (d/p/q/r)chisq R implementation, so look at, say,
+#' \code{\link{dchisq}} for details.
+#'
+#'
+#' @param x,q vector of quantiles.
+#' @param p vector of probabilities.
+#' @param n number of observations. If length(n) > 1, the length is
+#'   taken to be the number required.
+#' @param df degrees of freedom (non-negative, but can be
+#'   non-integer).
+#' @param ncp non-centrality parameter (non-negative).
+#' @param log,log.p logical; if TRUE, probabilities p are given as
+#'   log(p).
+#' @param lower.tail logical; if TRUE (default), probabilities are
+#'   P[X <= x] otherwise, P[X > x].
+#' @seealso \code{\link{dchisq}}; these functions just wrap the
+#'   (d/p/q/r)chisq functions.
+#' @name invchisq
+#' @examples
+#'
+#' s <- seq(0, 3, .01)
+#' plot(s, dinvchisq(s, 3), type = 'l')
+#'
+#' f <- function(x) dinvchisq(x, 3)
+#' q <- 2
+#' integrate(f, 0, q)
+#' (p <- pinvchisq(q, 3))
+#' qinvchisq(p, 3) # = q
+#' mean(rinvchisq(1e5, 3) <= q)
+#'
+#'
+#'
+#'
+#' f <- function(x) dinvchisq(x, 3, ncp = 2)
+#' q <- 1.5
+#' integrate(f, 0, q)
+#' (p <- pinvchisq(q, 3, ncp = 2))
+#' qinvchisq(p, 3, ncp = 2) # = q
+#' mean(rinvchisq(1e7, 3, ncp = 2) <= q)
+#'
+#'
+#'
+NULL
+
+
+
+
+#' @rdname invchisq
+#' @export
+dinvchisq <- function(x, df, ncp = 0, log = FALSE) {
+  log_f <- dchisq(1/x, df, ncp, log = TRUE) - 2*log(x)
+  if(log) return(log_f)
+  exp(log_f)
+}
+
+
+
+
+#' @rdname invchisq
+#' @export
+pinvchisq <- function(q, df, ncp = 0, lower.tail = TRUE, log.p = FALSE) {
+  pchisq(1/q, df, ncp, lower.tail = !lower.tail, log.p = log.p)
+}
+
+
+
+
+#' @rdname invchisq
+#' @export
+qinvchisq <- function(p, df, ncp = 0, lower.tail = TRUE, log.p = FALSE) {
+  qchisq(1-p, df, ncp, lower.tail = lower.tail, log.p = log.p)^(-1)
+}
+
+
+
+
+
+#' @rdname invchisq
+#' @export
+rinvchisq <- function(n, df, ncp = 0) {
+  1 / rchisq(n, df, ncp)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ R/invexp.R
@@ -0,0 +1,95 @@
+#' The Inverse Exponential Distribution
+#'
+#' Density, distribution function, quantile function and random
+#' generation for the inverse exponential distribution.
+#'
+#' The functions (d/p/q/r)invexp simply wrap those of the standard
+#' (d/p/q/r)exp R implementation, so look at, say,
+#' \code{\link{dexp}} for details.
+#'
+#'
+#' @param x,q vector of quantiles.
+#' @param p vector of probabilities.
+#' @param n number of observations. If length(n) > 1, the length is
+#'   taken to be the number required.
+#' @param rate degrees of freedom (non-negative, but can be
+#'   non-integer).
+#' @param log,log.p logical; if TRUE, probabilities p are given as
+#'   log(p).
+#' @param lower.tail logical; if TRUE (default), probabilities are
+#'   P[X <= x] otherwise, P[X > x].
+#' @seealso \code{\link{dexp}}; these functions just wrap the
+#'   (d/p/q/r)exp functions.
+#' @name invexp
+#' @examples
+#'
+#' s <- seq(0, 10, .01)
+#' plot(s, dinvexp(s, 2), type = 'l')
+#'
+#' f <- function(x) dinvexp(x, 2)
+#' q <- 3
+#' integrate(f, 0, q)
+#' (p <- pinvexp(q, 2))
+#' qinvexp(p, 2) # = q
+#' mean(rinvexp(1e5, 2) <= q)
+#'
+#' pinvgamma(q, 1, 2)
+#'
+#'
+#'
+NULL
+
+
+
+
+#' @rdname invexp
+#' @export
+dinvexp <- function(x, rate = 1, log = FALSE) {
+  log_f <- dexp(1/x, rate, log = TRUE) - 2*log(x)
+  if(log) return(log_f)
+  exp(log_f)
+}
+
+
+
+
+#' @rdname invexp
+#' @export
+pinvexp <- function(q, rate = 1, lower.tail = TRUE, log.p = FALSE) {
+  pexp(1/q, rate, lower.tail = !lower.tail, log.p = log.p)
+}
+
+
+
+
+#' @rdname invexp
+#' @export
+qinvexp <- function(p, rate = 1, lower.tail = TRUE, log.p = FALSE) {
+  qexp(1-p, rate, lower.tail = lower.tail, log.p = log.p)^(-1)
+}
+
+
+
+
+
+#' @rdname invexp
+#' @export
+rinvexp <- function(n, rate = 1) {
+  1 / rexp(n, rate)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
